a89069463e73b61617ca6a11db677d3b1fa9e3ef
SufiyanGolandaz <golandaz.sufiyan18@siesgst.ac.in>
2022-10-04 12:01:00 +0530

    First Automation code added to Git

---
+++ src/test/MyFirstAutomationScript.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstAutomationScript {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\safzalgo\\Downloads\\chromedriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://adactinhotelapp.com");
+		driver.findElement(By.id("username")).sendKeys("sufu12345");
+		driver.findElement(By.id("password")).sendKeys("sufu1234");
+		driver.quit();
+	}
+
+}
