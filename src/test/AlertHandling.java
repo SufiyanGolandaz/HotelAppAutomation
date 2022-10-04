5addf9199343120d53b071cb256ee119ac8c9a54
SufiyanGolandaz <golandaz.sufiyan18@siesgst.ac.in>
2022-10-04 16:37:24 +0530

    Done

---
+++ src/test/AlertHandling.java
@@ -0,0 +1,24 @@
+package test;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertHandling {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\safzalgo\\Downloads\\chromedriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		String url="https://demoqa.com/alerts";
+		driver.navigate().to(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("alertButton")).click();
+		driver.switchTo().alert().accept();//deal with alerts
+		driver.findElement(By.id("timerAlertButton")).click();
+		driver.wait(5000);
+		driver.switchTo().alert().accept();
+		driver.quit();
+	}
+
+}
